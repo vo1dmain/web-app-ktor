eb14150a492f41ace6328aa5a3bdad08cf3bc59f
Дмитрий Бирюков <51951001+vo1dmain@users.noreply.github.com>
2022-06-16 16:37:12 +0300

    Багфикс

--- src/main/kotlin/ru/penzgtu/web/app/data/repos/base/QnaRepoBase.kt
+++ src/main/kotlin/ru/penzgtu/web/app/data/repos/base/QnaRepoBase.kt
@@ -1,5 +1,6 @@
 package ru.penzgtu.web.app.data.repos.base
 
+import ru.penzgtu.web.app.data.dao.AnswerDao
 import ru.penzgtu.web.app.data.dao.PostDao
 import ru.penzgtu.web.app.data.dao.QuestionDao
 import ru.penzgtu.web.app.data.delegates.dao
@@ -8,4 +9,5 @@ import ru.penzgtu.web.app.data.repos.QnaRepo
 class QnaRepoBase : QnaRepo() {
     override val postDao by dao<PostDao>()
     override val questionDao by dao<QuestionDao>()
+    override val answerDao by dao<AnswerDao>()
 }
