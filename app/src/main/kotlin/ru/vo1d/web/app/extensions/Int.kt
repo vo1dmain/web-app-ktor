3f69c3c996e811ae9ef874a99e57e2408d9cab99
Дмитрий Бирюков <51951001+vo1dmain@users.noreply.github.com>
2022-07-30 02:46:05 +0300

    Форматирование

--- app/src/main/kotlin/ru/vo1d/web/app/extensions/Int.kt
+++ app/src/main/kotlin/ru/vo1d/web/app/extensions/Int.kt
@@ -2,7 +2,6 @@ package ru.vo1d.web.app.extensions
 
 import io.ktor.server.plugins.*
 
-fun Int.failIfNegative(): Int {
+fun Int.failIfNegative() = apply {
     if (this < 0) throw BadRequestException("Request parameter is less than zero")
-    return this
 }
