2576f42051ca65fc6039ecc6e692051300eea31b
Дмитрий Бирюков <51951001+vo1dmain@users.noreply.github.com>
2022-07-30 03:12:13 +0300

    Плагины отключены по умолчанию

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,8 @@ rootProject.name = "web-app-2"
 
 pluginManagement {
     plugins {
-        kotlin("jvm") version "1.7.10"
-        kotlin("plugin.serialization") version "1.7.10"
+        kotlin("jvm") version "1.7.10" apply false
+        kotlin("plugin.serialization") version "1.7.10" apply false
     }
 }
 
