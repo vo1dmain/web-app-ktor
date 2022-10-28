1b22c79d65004a94b7543ead0ef0a0bbb19ad0aa
Дмитрий Бирюков <51951001+vo1dmain@users.noreply.github.com>
2022-10-28 17:19:25 +0300

    Обновление Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,8 @@ rootProject.name = "web-app"
 
 pluginManagement {
     plugins {
-        kotlin("jvm") version "1.7.10" apply false
-        kotlin("plugin.serialization") version "1.7.10" apply false
+        kotlin("jvm") version "1.7.20" apply false
+        kotlin("plugin.serialization") version "1.7.20" apply false
     }
 }
 includeBuild("core")
